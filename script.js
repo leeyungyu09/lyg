4681210c8150cef1b4c2d47d4e1dbc20f831917f
leeyungyu09 <leeyungyu09@gmail.com>
2024-07-22 15:33:04 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let a;
+const b = 0;
+function func(){
+    let fo = document.getElementById('fo').value
+    fo = fo *5
+    document.getElementById('re').innerHTML = fo
+}
---
+++ script2.js
@@ -0,0 +1,9 @@
+function func(){
+    let fo = document.getElementById('fo').value
+    fo = fo *5
+    document.getElementById('re').innerHTML = fo
+}
+function func(){
+    let po = document.getElementById('po').value
+    document.getElementById('qw').innerHTML = po
+}
